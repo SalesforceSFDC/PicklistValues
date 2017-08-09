8ff9150634152565654cc689f3ec7e0883a45eca
Vuk Dukic <vuk.dukic@clara.com>
2017-08-09 11:53:50 -0700

    Update and rename PicklistValues.cmp.js to PicklistValuesCmp.js

--- PicklistValues.cmp.js
+++
@@ -1,7 +0,0 @@
-<aura:component >
-	
-    <aura:attribute name="sObjectName" type="String" />
-	  <aura:attribute name="fieldName" type="String" />
-	  <aura:attribute name="picklistValues" type="Object" />
-
-</aura:component>
